a650170111dc299e8669067ff0acfa7d183e1db0
GoblinCarcass <goblincarcass@protonmail.com>
2025-08-09 16:31:10 +0200

    Added camera rotation
    Sloppy, but works

--- recursiveminigames/scenes/player/player.gd
+++ recursiveminigames/scenes/player/player.gd
@@ -2,26 +2,31 @@ extends CharacterBody3D
 
 @onready var camera_rotator: Marker3D = $CameraRotator
 @onready var camera: Camera3D = $CameraRotator/Camera3D
+@onready var model: Node3D = $GirlTutorialTest
 
 # Mouse sensitivity
-@export var sens_horizontal = 0.05
-@export var sens_vertical = 0.05
+@export var sens_horizontal := 2
+@export var sens_vertical := 2
 
 const SPEED = 5.0
 const RUN_SPEED = 20
 const JUMP_VELOCITY = 4.5
 
+
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 
 func _input(event: InputEvent) -> void:
 		#Handle camera rotation
 	if event is InputEventMouseMotion:
-		camera_rotator.rotate_y(-event.relative.x * sens_horizontal)
-		camera.rotate_x(-event.relative.y * sens_vertical)
+		self.rotate_y(-event.relative.x * sens_horizontal / 100)
+		camera.rotate_x(-event.relative.y * sens_vertical / 100)
 		
 		#Limit camera rotation (no cartwheels lmao)
 		camera.rotation.x = clampf(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
