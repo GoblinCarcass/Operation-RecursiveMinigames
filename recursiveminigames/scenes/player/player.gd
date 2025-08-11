538b14defa65db31777fd6968fba0f4a96f89de5
GoblinCarcass <goblincarcass@protonmail.com>
2025-08-11 19:08:17 +0200

    Polished the sensitivity

--- recursiveminigames/scenes/player/player.gd
+++ recursiveminigames/scenes/player/player.gd
@@ -6,8 +6,8 @@ extends CharacterBody3D
 @onready var state: StateChart = %StateChart
 
 # Mouse sensitivity
-@export_range(0.5, 5, 0.5) var sens_horizontal: float
-@export_range(0.5, 5, 0.5) var sens_vertical: float
+@export_range(0.2, 5, 0.2) var sens_horizontal: float
+@export_range(0.2, 5, 0.2) var sens_vertical: float
 
 const WALK_SPEED = 5.0
 const RUN_SPEED = 20
@@ -27,8 +27,8 @@ func _input(event: InputEvent) -> void:
 
 func handle_camera_rotation(event: InputEvent):
 	if event is InputEventMouseMotion:
-		self.rotate_y(-event.relative.x * sens_horizontal / 100)
-		camera.rotate_x(-event.relative.y * sens_vertical / 100)
+		self.rotate_y(-event.relative.x * sens_horizontal / 500)
+		camera.rotate_x(-event.relative.y * sens_vertical / 500)
 		
 		#Limit camera rotation (no cartwheels lmao)
 		camera.rotation.x = clampf(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
