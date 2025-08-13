31e2206b3022feb7e8766722daa21c7a477051e2
GoblinCarcass <goblincarcass@protonmail.com>
2025-08-13 17:21:23 +0200

    Fixed crashing when torus rotation values are null

--- recursiveminigames/scenes/main/abominable_torus_of_ignorance.gd
+++ recursiveminigames/scenes/main/abominable_torus_of_ignorance.gd
@@ -1,10 +1,16 @@
 extends MeshInstance3D
 
-@export_range(0, 1, 0.05) var rot_x
-@export_range(0, 1, 0.05) var rot_y
-@export_range(0, 1, 0.05) var rot_z
+@export_range(0, 1, 0.05) var rot_x 
+@export_range(0, 1, 0.05) var rot_y 
+@export_range(0, 1, 0.05) var rot_z 
 
-
+func _ready() -> void:
+	if !rot_x:
+		rot_x = 0
+	if !rot_y:
+		rot_y = 0
+	if !rot_z:
+		rot_z = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 	self.rotate_x(rot_x / 100)
