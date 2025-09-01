bbeccecaa0a27a91dd6405f19b2883231e4445c8
GoblinCarcass <goblincarcass@protonmail.com>
2025-09-01 17:32:43 +0200

    Update signal_bus.gd
    Added warning ignores, because the signals are called externally anyway

--- recursiveminigames/scripts/globals/signal_bus.gd
+++ recursiveminigames/scripts/globals/signal_bus.gd
@@ -1,22 +1,33 @@
 extends Node
 
 # Game Manager
+@warning_ignore("unused_signal")
 signal scene_3d_changed
+@warning_ignore("unused_signal")
 signal scene_2d_changed
+@warning_ignore("unused_signal")
 signal scene_gui_changed
 
 # Dialog 
+@warning_ignore("unused_signal")
 signal dialog_started(id: String)
+@warning_ignore("unused_signal")
 signal dialog_acknowledged
+@warning_ignore("unused_signal")
 signal dialog_box_visibility_set(is_visible: bool)
 
 
 # Player
 # TODO: Figure out if I can set the movement mode outside of the SignalBus
+@warning_ignore("unused_signal")
 signal player_movement_mode_set(can_move: bool)
+@warning_ignore("unused_signal")
 signal player_can_rotate_camera_mode_set(can_rotate: bool)
 
 # UI
+@warning_ignore("unused_signal")
 signal crosshair_text_changed(text: String)
+@warning_ignore("unused_signal")
 signal crosshair_changed(crosshair: Texture2D)
+@warning_ignore("unused_signal")
 signal crosshair_visibility_changed(is_visible: bool)
