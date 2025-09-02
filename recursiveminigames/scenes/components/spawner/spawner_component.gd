9a777f1f7724479e0e998f4741a9f4418353b67a
GoblinCarcass <goblincarcass@protonmail.com>
2025-09-02 10:32:47 +0200

    Cleaned up the spawning code, added spawned entity array for future shenanigans

--- recursiveminigames/scenes/components/spawner/spawner_component.gd
+++ recursiveminigames/scenes/components/spawner/spawner_component.gd
@@ -6,16 +6,19 @@ signal entity_spawned(entity: Node)
 ## in a 2D scene it will load improperly and throw an error.
 ## [br][br]
 ## The dimensions are provided explicitly to avoid confusion and odd error handling.
-@export_enum("3D", "2D") var entity_dimensions: int
+@export_enum("3D", "2D") var dimensions: int
 
+var _last_spawned_entity: Node = null
+var _spawned_entites: Array[Node] = []
 var _spawn_points_2d: Array[Node2D] = []
 var _spawn_points_3d: Array[Node3D] = []
+
 
 func _ready() -> void:
 	# Initialize available spawners
 	
 	var children = self.get_children()
-	match entity_dimensions:
+	match dimensions:
 		0: # 3D
 			for child in children:
 				if child is not Node3D:
@@ -26,18 +29,42 @@ func _ready() -> void:
 		1: # 2D
 			for child in children:
 				if child is not Node2D:
-					assert(false, "The child of the SpawnerComponent has incorrect dimensions!
+					assert(false,
+					"The child of the SpawnerComponent has incorrect dimensions!
 					This Spawner only accepts 2D Nodes!")
 					continue
 				_spawn_points_2d.append(child)
 
 
-func spawn_entity(entity: Node, spawn_name: String):
+## Find a specific spawn point by it's name
+func _get_spawn_point(node_name: String, arr: Array) -> Node:
+	var spawn_point: Node = null
+	for i in arr:
+		if i.name != node_name:
+			continue
+		spawn_point = i
+		break
+	return spawn_point
+
+
+func _setup_entity_in_scene_tree(entity: Node, spawn_point: Node):
+	self.get_parent().add_child(entity)
+	_spawned_entites.append(entity)
+	_last_spawned_entity = entity
+	entity.position.x = spawn_point.position.x
+	entity.position.y = spawn_point.position.y
+	if dimensions == 0: #3D 
+		entity.position.z = spawn_point.position.z
+	entity.global_rotation = spawn_point.global_rotation
+	entity_spawned.emit(entity)
+
+
+func spawn_entity(entity: Node, spawn_name: StringName):
 	# Step 1: Get the desired entity
 	# Step 2: Get the desired destination
 	# Step 3: Ship the entity to the destination
 	# Simple, right?
-	match entity_dimensions:
+	match dimensions:
 		0: # 3D
 			var spawn_point: Node3D = _get_spawn_point(spawn_name, _spawn_points_3d)
 			 
@@ -49,12 +76,7 @@ func spawn_entity(entity: Node, spawn_name: String):
 				printerr("Spawnable entity has incorrent dimensions! It should inherit from Node3D!")
 				return
 			
-			self.get_parent().add_child(entity)
-			entity.position.x = spawn_point.position.x
-			entity.position.y = spawn_point.position.y
-			entity.position.z = spawn_point.position.z
-			entity.global_rotation = spawn_point.global_rotation
-			entity_spawned.emit(entity)
+			_setup_entity_in_scene_tree(entity, spawn_point)
 			
 		1: # 2D
 			var spawn_point: Node2D = _get_spawn_point(spawn_name, _spawn_points_2d)
@@ -67,19 +89,15 @@ func spawn_entity(entity: Node, spawn_name: String):
 				printerr("Spawnable entity has incorrent dimensions! It should inherit from Node2D!")
 				return
 			
-			self.get_parent().add_child(entity)
-			entity.position.x = spawn_point.position.x
-			entity.position.y = spawn_point.position.y
-			entity.global_rotation = spawn_point.global_rotation
-			entity_spawned.emit(entity)
+			_setup_entity_in_scene_tree(entity, spawn_point)
 
 
-## Find a specific spawn point by it's name
-func _get_spawn_point(node_name: String, arr: Array) -> Node:
-	var spawn_point: Node = null
-	for i in arr:
-		if i.name != node_name:
-			continue
-		spawn_point = i
-		break
-	return spawn_point
+func remove_entities():
+	for entity in _spawned_entites:
+		entity.queue_free()
+	_spawned_entites.clear()
+
+
+func remove_last_entity():
+	if null != _last_spawned_entity:
+		_last_spawned_entity.queue_free()
