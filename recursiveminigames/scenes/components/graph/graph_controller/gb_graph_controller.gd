e38ebc4e3f5fa99a181dae01077b0892418f41ee
GoblinCarcass <goblincarcass@protonmail.com>
2025-09-17 20:26:13 +0200

    Nuked th eprototype graph nodes
    It's time to redo it from scratch

--- recursiveminigames/scenes/components/graph/graph_controller/gb_graph_controller.gd
+++
@@ -1,6 +0,0 @@
-class_name GB_GraphManager extends Node2D
-
-var nodes_in_graph: Array[GB_GraphNode] = []
-
-# TODO: Collect nodes under the manager's control and add some game functionality
-# Idea: Add the "influence" hacking system where over time nodes that are being hacked become red
