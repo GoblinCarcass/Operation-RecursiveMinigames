77f651e2e77471ec00dd16081546dba2b6626187
GoblinCarcass <goblincarcass@protonmail.com>
2025-09-20 12:59:34 +0200

    Added indexing of the graph nodes
    Holy fucking shit it actually works absolute CODING

--- recursiveminigames/scenes/components/graph/graph_manager_2d/graph_manager_2d.gd
+++ recursiveminigames/scenes/components/graph/graph_manager_2d/graph_manager_2d.gd
@@ -1,13 +1,19 @@
 class_name GraphManager2D extends Node2D
 
-# Dictionary typing sucks some serious DICK
-# Fuck me, man
-var graph_connections: Dictionary[GraphNode2D, Array]
+var indexed_graph_nodes: Dictionary[int, GraphNode2D]
+var graph_connections: Dictionary[int, Array]
 
 func _ready() -> void:
-	var graph_nodes: Array[Node]  = get_children()
-	for node in graph_nodes:
+	# Initial index of the graph nodes
+	var node_array: Array[Node] = get_children()
+	for node in node_array:
 		if node is not GraphNode2D:
-			printerr("The " + node.name + " Node inside of the " + name + " GraphManager is of incorrect type")
+			printerr("The " + node.name + " Node inside of the " + name + " GraphManager is of incorrect type at the " + self.get_parent().name + " level")
 			continue
-		graph_connections[node as GraphNode2D] = node.get_connections()
+		indexed_graph_nodes[node.get_index()] = node
+	
+	# Save the connections between the nodes
+	for key in indexed_graph_nodes.keys():
+		var node: GraphNode2D = indexed_graph_nodes[key] 
+		graph_connections[key] = node.connections
+	
--- recursiveminigames/scenes/components/graph/graph_node_2d/graph_node_2d.gd
+++ recursiveminigames/scenes/components/graph/graph_node_2d/graph_node_2d.gd
@@ -5,38 +5,29 @@ class_name GraphNode2D extends Area2D
 @onready var sprite_2d: Sprite2D = %Sprite2D
 
 @export var connections: Array[GraphNode2D] = []:
-	get: return connections
-	set(value): connections = value
+	get = get_connections
+func get_connections() -> Array[GraphNode2D]:
+	return connections
+	
 @export_group("Exports")
 @export var shape_export: Shape2D = null
 @export var sprite_export: Texture2D = null
 
-
-func _init() -> void:
-	print(collision_shape_2d)
-
+var index: int = -1
 
 func _ready() -> void:
-	print(collision_shape_2d)
-	if !Engine.is_editor_hint():
-		if null != shape_export:
-			collision_shape_2d.shape = shape_export
-			pass
-		if null != sprite_export:
-			sprite_2d.texture = sprite_export
-			pass
-		queue_redraw()
+	if null != shape_export:
+		collision_shape_2d.shape = shape_export
+	if null != sprite_export:
+		sprite_2d.texture = sprite_export
+	queue_redraw()
 
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
+		if collision_shape_2d.shape != shape_export:
+			collision_shape_2d.shape = shape_export
+		if sprite_2d.texture != sprite_export:
+			sprite_2d.texture = sprite_export
+			queue_redraw()
+	else:
 		pass
-		# TODO: Why in the fuck does this not work properly?
-		# It pushes an error because collision_shape_2d hasn't loaded yet, but how in the fuck am I supposed
-		# to load it in the editor script? It makes no sense. 
-		
-		#if shape_export != collision_shape_2d.shape and collision_shape_2d.is_node_ready():
-			#collision_shape_2d.shape  = shape_export
-			#queue_redraw()
-		#if sprite_export != sprite_2d.texture and sprite_2d.is_node_ready():
-			#sprite_2d.texture = sprite_export
-			#queue_redraw()
