6ac52547a82406daaa41c746b7a0d1c2ef562801
GoblinCarcass <goblincarcass@protonmail.com>
2025-10-21 17:50:03 +0200

    Add a hexagonal player scene
    TODO: Add movement script
    REMEMBER: Moving by a mouse, check for collisions to see if you can go into the spot

--- recursiveminigames/scenes/levels/2d/DemoHackingMinigame/demo_hacking_minigame.gd
+++ recursiveminigames/scenes/levels/2d/DemoHackingMinigame/demo_hacking_minigame.gd
@@ -1,10 +1,28 @@
 extends Node2D
 
-@onready var spawner_component: SpawnerComponent = $SpawnerComponent
-const HEX_PLAYER = preload(Paths.TEST_PLAYER_HEX)
-var player = null
+@onready var tile_map_layer: TileMapLayer = $Visuals/TileMap/TileMapLayer
+var tile_set: TileSet = null
+const PLAYER_HEX: PackedScene = preload(Paths.TEST_PLAYER_HEX)
+const STARTING_TILE: Vector2i = Vector2i(2, 2)
+
+var player: CharacterBody2D = PLAYER_HEX.instantiate()
 
 func _ready() -> void:
-	player = HEX_PLAYER.instantiate()
-	if not player == null:
-		spawner_component.spawn(player, "player_start")
+	tile_set = tile_map_layer.get_tile_set()
+	start_game()
+	player.coords = STARTING_TILE
+	print(str(tile_set.tile_size.x) + " " + str(tile_set.tile_size.y))
+
+
+func start_game():
+	# Load player into the starting tile on the map
+	var starting_coords = tile_map_layer.map_to_local(STARTING_TILE)
+	player.global_position.x = starting_coords.x
+	player.global_position.y = starting_coords.y + (starting_coords.y / 2)
+	self.add_child(player)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_type():
+		if event is InputEventMouse:
+			pass
--- recursiveminigames/scripts/globals/paths.gd
+++ recursiveminigames/scripts/globals/paths.gd
@@ -1,8 +1,9 @@
 extends Node
 
 # Entities
-const TEST_PLAYER_3D := "res://scenes/entities/player_3d/player_3d.tscn"
+const TEST_PLAYER_3D := "uid://ceo7bsiuu6vs"
 const TEST_PLAYER_HEX := "uid://blyfy4ap6lvv2"
+
 # Levels
-const DAM_LEVEL_V1 := "res://scenes/levels/3d/dam_level_v1/dam_level_v1.tscn"
-const TORUS_TEST_LEVEL := "res://scenes/levels/3d/dam_test_level/dam_test_level.tscn"
+const DAM_LEVEL_V1 := "uid://j51a8iiipcuy"
+const TORUS_TEST_LEVEL := "uid://dltmtdkqo6yid"
